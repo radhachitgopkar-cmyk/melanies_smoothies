c534ec2becc22bb00d109ab463c02ce2b023b646
radhachitgopkar-cmyk <radhachitgopkar@gmail.com>
2026-07-29 12:21:35 -0500

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -1,6 +1,7 @@
 # Import Python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests 
 
 # Get the current credentials
 cnx = st.connection("snowflake")
@@ -38,6 +39,8 @@ ingredients_string = ""
 
 for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + " "
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Submit button
 if st.button("Submit Order"):
@@ -60,9 +63,6 @@ if st.button("Submit Order"):
     # Confirmation message
     st.success("✅ Your Smoothie is ordered, " +name_on_order + "!")
 
-import requests  
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
 )  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
