91d3bcab73f4e559e97b6e5784b4940c400f7b70
radhachitgopkar-cmyk <radhachitgopkar@gmail.com>
2026-07-29 12:15:06 -0500

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -64,4 +64,4 @@ import requests
 smoothiefroot_response = requests.get(
     "https://my.smoothiefroot.com/api/fruit/watermelon"
 )  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
